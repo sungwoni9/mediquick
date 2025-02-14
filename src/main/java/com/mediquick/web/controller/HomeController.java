b7f378742bd3ca810ddf8145585d1e4203831cb4
sungwoni9 <hurdang99@naver.com>
2025-02-14 15:00:26 +0900

    Create Project

---
+++ src/main/java/com/mediquick/web/controller/HomeController.java
@@ -0,0 +1,28 @@
+package com.mediquick.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/header")
+    public String header(){
+        return "modle/header";
+    }
+
+    @GetMapping("/footer")
+    public String footer(){
+        return "modle/footer";
+    }
+
+    @GetMapping("/error")
+    public String error(){
+        return "error";
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/module/footer.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: hurda
+  Date: 2025-02-14
+  Time: 오후 2:27
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+</head>
+<body>
+<footer></footer>
+<address>서울시 서초구</address>
+</body>
+</html>
