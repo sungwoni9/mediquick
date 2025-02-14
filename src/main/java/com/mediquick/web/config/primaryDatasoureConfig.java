d9f7299bd9867b4d17707a822e3c370ac3edb53a
sungwoni9 <hurdang99@naver.com>
2025-02-14 18:21:22 +0900

    Add DatasourceConfig : MySQL and Oracle

---
+++ src/main/java/com/mediquick/web/config/primaryDatasoureConfig.java
@@ -0,0 +1,80 @@
+package com.mediquick.web.config;
+
+import jakarta.persistence.EntityManagerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@EnableJpaRepositories(
+        basePackages = "com.mediquick.web.primary",
+        entityManagerFactoryRef = "primaryEntityManager",
+        transactionManagerRef = "primaryTransactionManager"
+)
+@EnableTransactionManagement
+class primaryDatasoureConfig {
+
+    @Value("${spring.datasource.primary.url}")
+    private String url;
+
+    @Value("${spring.datasource.primary.username}")
+    private String username;
+
+    @Value("${spring.datasource.primary.password}")
+    private String password;
+
+    @Value("${spring.datasource.primary.driver-class-name}")
+    private String driverClassName;
+
+    @Primary
+    @Bean(name="primaryDataSource")
+    public DataSource dataSource() {
+      return DataSourceBuilder.create()
+              .url(url)
+              .username(username)
+              .password(password)
+              .driverClassName(driverClassName)
+              .build();
+    }
+
+    @Primary
+    @Bean(name="primaryEntityManager")
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        vendorAdapter.setGenerateDdl(true);
+
+        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+        factory.setJpaVendorAdapter(vendorAdapter);
+        factory.setPackagesToScan("com.mediquick.web.primary");
+        factory.setDataSource(dataSource);
+
+        Map<String, Object> properties = new HashMap<>();
+        properties.put("hibernate.hbm2ddl.auto", "none");
+        properties.put("hibernate.show_sql", true);
+        properties.put("hibernate.format_sql", true);
+        factory.setJpaPropertyMap(properties);
+
+        return factory;
+    }
+
+    @Primary
+    @Bean(name="primaryTransactionManager")
+    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+
+        JpaTransactionManager txManager = new JpaTransactionManager();
+        txManager.setEntityManagerFactory(entityManagerFactory);
+        return txManager;
+    }
+}
