c14dafdd691768b3858d1195f3076a616cb1f789
sungwoni9 <hurdang99@naver.com>
2025-02-14 18:22:49 +0900

    Add Study Service

---
+++ src/main/java/com/mediquick/web/secondary/study/service/StudyService.java
@@ -0,0 +1,19 @@
+package com.mediquick.web.secondary.study.service;
+
+import com.mediquick.web.secondary.study.domain.Study;
+import com.mediquick.web.secondary.study.domain.StudyRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class StudyService {
+    private final StudyRepository studyRepository;
+
+    public List<Study> findStudyAll(){
+        return studyRepository.findAll();
+    }
+
+}
