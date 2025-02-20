28a5aa0eae9f5f66f16671d5b3e86cba4355a4bf
ohserin <dhtpfls321@gmail.com>
2025-02-20 18:08:00 +0900

    Add finding domain and service

---
+++ src/main/java/com/mediquick/web/primary/finding/domain/FindingRequestDto.java
@@ -0,0 +1,29 @@
+package com.mediquick.web.primary.finding.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class FindingRequestDto {
+    private long id;
+    private boolean isNormal;
+    private String lesionLocation;
+    private String lesionSize;
+    private int lesionCount;
+    private String morphology;
+    private String additionalFindings;
+    private String possibleDiagnosis;
+    private String clinicalSignificance;
+    private boolean recommendedStudies;
+    private String comparisonStudies;
+    private String additionalComment;
+    private String additionalNotes;
+    private String radiologistName;
+    private int urgencyLevel;
+    private int reportStatus;
+}
