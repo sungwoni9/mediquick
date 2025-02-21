20d0393aafbf457f45082a14665b860284a76672
ghj0595 <ghj0595@naver.com>
2025-02-21 15:20:23 +0900

    Modify Admin Controller

--- src/main/java/com/mediquick/web/controller/AdminController.java
+++ src/main/java/com/mediquick/web/controller/AdminController.java
@@ -1,21 +1,23 @@
 package com.mediquick.web.controller;
 
+import com.mediquick.web.primary.logs.domain.Log;
+import com.mediquick.web.primary.logs.service.LogService;
 import com.mediquick.web.primary.userinfo.domain.UserInfo;
 import com.mediquick.web.primary.userinfo.service.UserInfoService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
+@RequiredArgsConstructor
 @Controller
 public class AdminController {
 
     private final UserInfoService userInfoService;
-
-    public AdminController(UserInfoService userInfoService) {
-        this.userInfoService = userInfoService;
-    }
+    private final LogService logService;
 
     @GetMapping("/management")
     public String management() {
@@ -23,7 +25,12 @@ public class AdminController {
     }
 
     @GetMapping("/logList")
-    public String logList() {
+    public String getUserLogs(@RequestParam String username, Model model) {
+
+        List<Log> logs = logService.findLogsByUsername(username);
+
+        model.addAttribute("logs", logs);
+
         return "logList";
     }
 
