71ba35321ebb958b0a74a20c71c1aae694d18141
ghj0595 <ghj0595@naver.com>
2025-02-21 12:14:38 +0900

    Modify User Rest Controller

--- src/main/java/com/mediquick/web/controller/UserRestController.java
+++ src/main/java/com/mediquick/web/controller/UserRestController.java
@@ -3,6 +3,7 @@ package com.mediquick.web.controller;
 import com.mediquick.web.primary.user.domain.User;
 import com.mediquick.web.primary.user.domain.UserRequestDto;
 import com.mediquick.web.primary.user.service.UserService;
+import com.mediquick.web.primary.userinfo.domain.UserInfo;
 import com.mediquick.web.primary.userinfo.service.UserInfoService;
 import com.mediquick.web.primary.userrole.service.UserRoleService;
 import com.mediquick.web.security.JwtUtil;
@@ -15,7 +16,10 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RequestMapping("user")
@@ -85,5 +89,11 @@ public class UserRestController {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
                     .body(new ResponseDto(HttpStatus.UNAUTHORIZED.value(), "Invalid username or password"));
         }
+    }
+
+    public String getAllUsers(Model model) {
+        List<UserInfo> users = userInfoService.findAll();
+        model.addAttribute("users", users);
+        return "users";
     }
 }
