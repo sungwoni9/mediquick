4de1c8524941f65f335171d430f03d20dc439202
sungwoni9 <hurdang99@naver.com>
2025-02-24 17:38:28 +0900

    Modify StudyController for security context usage 2025.02.24 1. SecurityContextHolder 사용하여 현재 사용자 정보 조회 추가 2. 요청 URL 변경: /list → /list/studyList 3. 뷰 경로 수정: study_list/studyList → list/studyList

--- src/main/java/com/mediquick/web/controller/StudyController.java
+++ src/main/java/com/mediquick/web/controller/StudyController.java
@@ -2,21 +2,25 @@ package com.mediquick.web.controller;
 
 import com.mediquick.web.secondary.study.service.StudyService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequiredArgsConstructor
-@RequestMapping("/study")
+@RequestMapping
 @Controller
 public class StudyController {
 
     private final StudyService studyService;
 
-    @GetMapping("/list")
+    @GetMapping("/list/studyList")
     public String list(Model model) {
+
+        String currentUsername = SecurityContextHolder.getContext().getAuthentication().getName();
+
         model.addAttribute("studies", studyService.findStudyAll());
-        return "study_list/studyList";
+        return "list/studyList";
     }
 }
