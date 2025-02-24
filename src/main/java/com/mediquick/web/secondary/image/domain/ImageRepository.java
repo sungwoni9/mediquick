821f50227a20ffea8d04a845c776acd8ba3a2473
SG5143 <sun5143a@gmail.com>
2025-02-24 17:44:03 +0900

    Feat: Add image query by study, series, and image key 2025.02.24 1. ImageRepository에 새로운 쿼리 메서드 추가 2. studykey, serieskey, imagekey로 이미지 조회

--- src/main/java/com/mediquick/web/secondary/image/domain/ImageRepository.java
+++ src/main/java/com/mediquick/web/secondary/image/domain/ImageRepository.java
@@ -1,6 +1,7 @@
 package com.mediquick.web.secondary.image.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,5 +10,7 @@ import java.util.List;
 public interface ImageRepository extends JpaRepository<Image, Integer> {
 
     List<Image> findImageByStudykey(int studykey);
+
+    Image findByStudykeyAndSerieskeyAndImagekey(int studykey, int serieskey, int imagekey);
 
 }
