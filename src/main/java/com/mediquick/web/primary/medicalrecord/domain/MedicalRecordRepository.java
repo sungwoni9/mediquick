56d4d68de5fb52b0997921b896edd381563c6816
sungwoni9 <hurdang99@naver.com>
2025-02-24 17:40:48 +0900

    Add sorting support 2025.02.24 1. findAll 메서드 추가하여 정렬 지원

--- src/main/java/com/mediquick/web/primary/medicalrecord/domain/MedicalRecordRepository.java
+++ src/main/java/com/mediquick/web/primary/medicalrecord/domain/MedicalRecordRepository.java
@@ -1,9 +1,13 @@
 package com.mediquick.web.primary.medicalrecord.domain;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface MedicalRecordRepository extends JpaRepository<MedicalRecord, Integer> {
     List<MedicalRecord> findByPid(String pid);
+
+    List<MedicalRecord> findAll(Sort sort);
+
 }
