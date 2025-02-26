d40f5906e8a3bd68018dcc7921921ac4da72fcf5
ohserin <dhtpfls321@gmail.com>
2025-02-26 18:04:23 +0900

    Add study retrieval by studykey in StudyRestController 2025.02.26 1. studykey를 이용한 특정 스터디 조회 API 추가

--- src/main/java/com/mediquick/web/controller/StudyRestController.java
+++ src/main/java/com/mediquick/web/controller/StudyRestController.java
@@ -1,9 +1,11 @@
 package com.mediquick.web.controller;
 
+import com.mediquick.web.secondary.study.domain.Study;
 import com.mediquick.web.secondary.study.service.StudyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,5 +19,17 @@ public class StudyRestController {
     @GetMapping({"","/"})
     public ResponseEntity<?> getStudies(){
         return ResponseEntity.ok(studyService.findStudyAll());
+    }
+
+    @GetMapping("/{studykey}")
+    public ResponseEntity<Study> getStudyByStudyKey(@PathVariable("studykey") int studyKey){
+
+        Study study = studyService.findStudyByStudykey(studyKey);
+
+        if(study == null){
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(study);
     }
 }
