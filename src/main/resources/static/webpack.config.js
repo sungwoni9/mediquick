0b0501fe39c0fbd3185b6933e44f9fcb5ab0459c
SG5143 <sun5143a@gmail.com>
2025-02-28 18:38:13 +0900

    Chore: Remove unnecessary Webpack config code 2025.02.28

--- src/main/resources/static/webpack.config.js
+++ src/main/resources/static/webpack.config.js
@@ -2,38 +2,33 @@ const path = require('path');
 
 module.exports = {
     entry: {
-        index: './index.js',
+        // 'viewer'라는 이름으로 './script/viewer/viewer.js'를 진입점으로 설정
+        test: './index.js',
         viewer: './script/viewer/viewer.js'
     },
-    optimization: {
-        splitChunks: {
-            chunks: 'all', // 벤더와 앱 코드를 분리
-        },
-    },
     output: {
-        filename: '[name].[contenthash].bundle.js', // 캐싱 최적화
+        // 출력 파일 이름 패턴. '[name]'은 entry에서 정의한 키(index, viewer)가 들어감
+        filename: '[name].bundle.js',
+        // 출력 파일이 저장될 절대 경로
         path: path.resolve(__dirname, 'dist'),
-        chunkFilename: '[name].[chunkhash].chunk.js', // 청크 파일
+        // library: {
+        //     name: 'Mediquick',
+        //     type: 'umd'
+        // }
+    },
+    resolve: {
+      fallback: {
+          "fs": false,
+          "path": require.resolve("path-browserify")
+      }
     },
     module: {
         rules: [
             {
-                test: /\.wasm$/,
+                test: /\.wasm/,
                 type: 'asset/resource',
-            },
-        ],
+            }
+        ]
     },
-    resolve: {
-        fallback: {
-            "fs": false,
-            "path": require.resolve("path-browserify"),
-            "a": false,
-            "env": false,
-            "wasi_snapshot_preview1": false,
-        },
-    },
-    experiments: {
-        asyncWebAssembly: true,
-    },
-    mode: 'development'
+    mode: "development"
 };
