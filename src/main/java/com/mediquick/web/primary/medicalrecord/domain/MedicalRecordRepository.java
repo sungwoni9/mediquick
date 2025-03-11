f06bd9a26b6015ba5331655960901a045e67e845
sungwoni9 <hurdang99@naver.com>
2025-03-11 17:47:06 +0900

    feat: Add method to find medical records by study key 2025-03-11
    MedicalRecordRepository에 findByStudykey(Integer studykey) 메서드 추가.
    MedicalRecordService에 findByStudyKey(Integer studyKey) 메서드 추가.
    의료 기록을 studyKey 기반으로 조회할 수 있도록 기능 확장.

--- src/main/java/com/mediquick/web/primary/medicalrecord/domain/MedicalRecordRepository.java
+++ src/main/java/com/mediquick/web/primary/medicalrecord/domain/MedicalRecordRepository.java
@@ -4,9 +4,13 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface MedicalRecordRepository extends JpaRepository<MedicalRecord, Integer> {
     List<MedicalRecord> findByPid(String pid);
 
     List<MedicalRecord> findAll(Sort sort);
+
+    Optional<MedicalRecord> findByStudykey(Integer studykey);
+
 }
